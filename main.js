5034f2771e78f64b405761d01d5194b4bd840b61
gersonhacker009@gmail.com <gersonhacker009@gmail.com>
2024-06-05 16:12:09 +0200

    subir tdos arquivos do meu site

---
+++ main.js
@@ -0,0 +1,23 @@
+function s(){
+  window.location.href="https://wa.me//+258840186884"
+  alert("falar com suporte...")
+}
+
+function ola(){
+  window.location.href="e.html"
+  
+}
+function p(){
+  window.location.href="p.html"
+  
+}
+
+
+function ae() {
+  window.location.href = "ea.html"
+
+}
+function b() {
+  window.location.href = "b.html"
+
+}
